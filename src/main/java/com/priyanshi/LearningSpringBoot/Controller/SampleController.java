5166cccb28b5aa3a6f71859fd312e8aef0910361
priyanshi1399 <priyanshitomar001@gmail.com>
2025-08-15 01:52:07 +0530

    Path Variable

--- src/main/java/com/priyanshi/LearningSpringBoot/Controller/SampleController.java
+++ src/main/java/com/priyanshi/LearningSpringBoot/Controller/SampleController.java
@@ -1,14 +1,19 @@
 package com.priyanshi.LearningSpringBoot.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.DataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.beans.PropertyEditorSupport;
 
 @RestController
 @RequestMapping(value="/api")
 public class SampleController {
 
+    @InitBinder
+    protected void initBinder(DataBinder binder){
+        binder.registerCustomEditor(String.class, "firstName" , new FirstNamePropertyEditor());
+    }
     @GetMapping(path="/fetchUser")
     public String getUserDetails(@RequestParam (name="firstName") String firstName,
                                   @RequestParam(name="lastName" ,required=false)String lastName,
@@ -16,4 +21,18 @@ public class SampleController {
         return "fetching and returning user details based on firstName=" +firstName +  "lastName ="+ lastName +"and age is "+age;
     }
 
+    @GetMapping(path="/fetchUser/{firstName}")
+    //@ResponseBody
+    public String getDetails(@PathVariable (value="firstName") String firstName){
+        return "fetching and returning user details based on the firstName =" +firstName;
+    }
+
 }
+
+ class FirstNamePropertyEditor extends PropertyEditorSupport{
+    @Override
+
+     public void setAsText(String text) throws IllegalArgumentException{
+        setValue(text.trim().toLowerCase());
+    }
+ }
